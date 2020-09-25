d0a36b4d649799ba4c2dff0ac14a05f5f48f8f12
Petrik Lajos Szg <info@petrik.hu>
2020-09-25 11:46:27 +0200

    saját harcos bekérése

--- HarcosProjekt/Program.cs
+++ HarcosProjekt/Program.cs
@@ -10,16 +10,22 @@ namespace HarcosProjekt
     {
         static void Main(string[] args)
         {
-           Harcos egyikHarcos = new Harcos("Erős János", 1);
+            Console.Write("Adja meg a nevét a harcosának: ");
+            string harcosnev = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Milyen státusza legyen? (1-3): ");
+            int status = Convert.ToInt32(Console.ReadLine());
+           Harcos foHarcos = new Harcos(harcosnev, status);
           
             List<Harcos> harcosLista = new List<Harcos>();
            Harcos kihivo2= new Harcos("Turbo Titán", 2);
            Harcos kihivo3 = new Harcos("Vas Ember", 3);
            Harcos kihivo4 = new Harcos("Kovács Ádám", 1);
+
             harcosLista.Add(kihivo2);
             harcosLista.Add(kihivo3);
             harcosLista.Add(kihivo4);
-            Console.WriteLine(egyikHarcos);
+
             Console.WriteLine(kihivo2);
             Console.WriteLine(kihivo3);
             Console.WriteLine(kihivo4);
